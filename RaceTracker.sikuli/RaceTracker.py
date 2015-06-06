a2b3f7dbf1d361cbdd7774b8d771914cbbaf4e57
jbzdarkid <jbzdarkid@gmail.com>
2015-06-06 01:11:12 -0400

    Rewrite, with a bit more intelligence. Region selection could use some work, though.

--- RaceTracker.sikuli/RaceTracker.py
+++ RaceTracker.sikuli/RaceTracker.py
@@ -10,58 +10,67 @@
 #		- Scenario:
 #		- Runner A lists frame A. Runner B identifies frame A, but is in fact ahead of runner A.
 #		- Similary, Runner A identifies frame B, and is correctly behind.
-#		- Solution is probably to double-key (i.e. 2+ continuous frames) 
-# General idea is to create an array of frames (challenges) and for each thread/runner to have an associated list of times (responses)
-# Both of these should be read/write safe, using a semaphore...? Possibly threads will make local copies.
-# We're going need to do complete challenges to reconstruct order, rather than allowing only the leader to challenge.
-# 
+#		- Solution:
+#		Maintain a list of frames, captured every {30 sec}. For 1 minute, watch for frame 0 {0-60 sec delay}.
+#		For the next minute, watch for frame 1 {30-90 sec delay}. Etc, etc. Delays are calculated twice as slowly, but twice as accurately.
 
 from time import time
-from threading import thread, lock
+from threading import Lock, Thread
+import sys
 
+Settings.WaitScanRate = 60 # 60 scans per sec, this is 20x faster than normal!
 setThrowException(False) # If a match fails, returns None rather than raising exception FindFailed
-challenges = []
-responses = [[]]
-challenge_lock = Lock() # Used when creating a challenge.
-start_time = time()
-threshold = 5*60 # 5 minute threshold
-threads = []
-keep_alive = True
-
-def create_challenge(region, index):
-	challenge_lock.acquire()
-	challenges.append(capture(region)) # Current frame
-	time = time()
-	for i in range(len(responses)):
-		if i == index:
-			r.append(time)
-		else:
-			r.append(None)
-	challenge_lock.release()
-	return time
-
-def t(name, x, y, w, h, index):
-	responses.append([])
+ACCURACY = int(input('Frequency of checkpoints (image captures), in seconds.\nSuggested is 30: '))
+n = int(input('Number of runners: '))
+regions = [None]*n
+popup('For each region, hover your mouse over a corner, press caps lock,\nmove to the other corner, and then release caps lock.')
+for region in regions
+	while (!Env.isLockOn(Key.CAPS_LOCK)):
+		sleep(0.1)
+	l1 = Env.getMouseLocation()
+	while (Env.isLockOn(Key.CAPS_LOCK)):
+		sleep(0.1)
+	l2 = Env.getMouseLocation()
+	x = l1.getX()
+	y = l1.getY()
+	if l2.getX() < x:
+		x = l2.getX()
+		w = l1.getX()-x
+	else:
+		w = l2.getX()-x
+	if l2.getY() < y:
+		y = l2.getY()
+		h = l1.getY()-y
+	else:
+		h = l2.getY()-y
 	region = Region(x, y, w, h)
-	region.setAutoWaitTimeout(1) # Seconds
-	last_response = create_challenge(region, index)
-	while keep_alive:
+	region.highlight()
+
+for region in regions:
+	region.highlight() # De-highlight
+
+challenges = [[] for _ in range(n)]
+responses = [[]]
+threads = [None]*n
+# Get regions w/ mouse clicks, highlight to confirm
+start_time = time()
+
+for i in range(n):
+	threads[i] = Thread(target=watch, kwargs={'num':i})
+
+while (True): # This generates the comparison frames.
+	for i in range(len(regions)):
+		#challenges[i].append(regions[i].capture())
+		print ''
+	sleep(ACCURACY)
+
+def watch(num):
+	region = regions[num]
+	while (True):
+		c = 0
 		match = None
-		i = 0
-		while (match is None and time()-last_response < threshold*2):
-			active_challenges = 0
-			for i in range(len(challenges)):
-				if responses[index][i] is not None: # Challenge already completed
-					continue
-				active_challenges += 1
-				if active_challenges > len(threads)*2: # Don't consider challenges that are too recent. ***Warning: Limits number of acceptable failures.
-					break
-				match = region.wait(challenges[i])
-		responses[index][i] = time() # Challenge completed
-		if time() - last_response > threshold:
-			last_response = create_challenge(region, index)
-
-# Get the regions
-# Start threads
-# On a timer(?) update a spaghetti graph.
-
+		while (match == None):
+			challenge = challenges[c]
+			match = region.wait(challenge, ACCURACY*2)
+			c += 1
+		responses[num][c-1] = time()
