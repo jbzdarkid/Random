e5ebe18002c33ca3e7a4a786330af2e8693b1cc5
jbzdarkid <jbzdarkid@gmail.com>
2015-06-13 21:06:44 -0400

    No longer reading file in parts

--- mardek_sol_reader.py
+++ mardek_sol_reader.py
@@ -10,23 +10,18 @@ def to_hex(byte):
 	p2 = byte%16
 	return digits[p1] + digits[p2]
 
-signal = ['70', '6C', '61', '79', '74', '69', '6D', '65'] # playtime
-signal += ['08', '00', '00', '00', '03'] # Array of length 3, according to AMF0 format
+signal = 'playtime\x08\x00\x00\x00\x03'
 
-for i in range(70):
+for i in range(1):
 	try:
 		f = open(save_file_dir+'MARDEKv3__sg_{:d}.sol'.format(i))
-		data = []
-		
-		byte = f.read(1)
-		while byte != '':
-			data.append(to_hex(byte))
-			byte = f.read(1)
-		for j in range(len(data)-len(signal)):
-			if data[j:j+len(signal)] == signal:
-				hex = ''.join(data[j+len(signal):j+len(signal)+36]).decode('hex')
+		file = f.read()
+		f.close()
+
+		for j in range(len(file)-len(signal)):
+			if file[j:j+len(signal)] == signal:
+				hex = ''.join(file[j+len(signal):j+len(signal)+36])
 				h, m, s = unpack('>4xd4xd4xd', hex)
 				print 'Time for save file {:d}:\t{:02.0f}:{:02.0f}:{:02.0f}'.format(i+1,h,m,s)
-		f.close()
 	except IOError:
 		continue
