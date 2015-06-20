ba7c5bd1b975a03c5fe2cf0b1cd470edf3524b37
jbzdarkid <jbzdarkid@gmail.com>
2015-06-20 17:45:50 -0400

    Tracking regions are now larger

--- RaceTracker.sikuli/RaceTracker.py
+++ RaceTracker.sikuli/RaceTracker.py
@@ -15,14 +15,21 @@
 #		For the next minute, watch for frame 1 {30-90 sec delay}. Etc, etc. Delays are calculated twice as slowly, but twice as accurately.
 
 from time import time
-from threading import Lock, Thread
+from threading import Thread
 
 Settings.WaitScanRate = 30 # 30 scans per sec, this is 10x faster than normal!
 setThrowException(False) # If a match fails, returns None rather than raising exception FindFailed
 ACCURACY = int(input('Frequency of checkpoints (image captures), in seconds.\nSuggested is 30: '))
+REDUNDANCY = 2 # Also effects timeliness of information. If a runner is 10 minutes behind, and this value is at 2,
+# then this information will be discovered after 20 minutes.
 n = int(input('Number of runners: '))
 regions = []
 popup('For each region, hover your mouse over a corner, press caps lock,\nmove to the other corner, and then release caps lock.')
+App.focus('Google Chrome')
+min_width = Screen().getW()
+min_height = Screen().getH()
+max_width = 0
+max_height = 0
 for i in range(n):
 	while (not Env.isLockOn(Key.CAPS_LOCK)):
 		sleep(0.1)
@@ -42,12 +49,28 @@ for i in range(n):
 		h = l1.getY()-y
 	else:
 		h = l2.getY()-y
+	if min_width > w:
+		min_width = w
+	if max_width < w:
+		max_width = w
+	if min_height > h:
+		min_height = h
+	if max_height < h:
+		max_height = h
 	region = Region(x, y, w, h)
-	region.highlight()
-	regions.append(region)
+	region.highlight() # Highlight
+	regions.append([region])
 sleep(1)
-for region in regions:
-	region.highlight() # De-highlight
+for i in range(n):
+	regions[i][0].highlight() # De-highlight
+	error = 0
+	if error < max_width - regions[i][0].getW():
+		error = max_width - regions[i][0].getW()
+	if error < max_height - regions[i][0].getH():
+		error = max_height - regions[i][0].getH()
+	regions[i].append(error)
+	regions[i][0].setW(min_width)
+	regions[i][0].setH(min_height)
 
 challenges = [[] for _ in range(n)]
 responses = [[]]
@@ -56,13 +79,13 @@ threads = []
 start_time = time()
 
 def watch(num):
-	region = regions[num]
+	region = regions[num][0].nearby(regions[num][1])
 	while (True):
 		c = 0
 		match = None
 		while (match == None):
 			challenge = challenges[c]
-			match = region.wait(challenge, ACCURACY*2)
+			match = Screen().wait(challenge, ACCURACY*REDUNDANCY) #?
 			c += 1
 		responses[num][c-1] = time()
 		# I'm not getting any matches.
@@ -72,8 +95,10 @@ for i in range(n):
 
 for _ in range(10): # This generates the comparison frames.
 	for i in range(len(regions)):
-		challenges[i].append(Screen().capture(regions[i]))
+		challenges[i].append(Screen().capture(regions[i][0]))
 	sleep(ACCURACY)
 
-print challenges
-print responses
+import pprint
+pp = pprint.PrettyPrinter(indent=2)
+pp.pprint(challenges)
+pp.pprint(responses)
