0e1a4e84cb94eb7698f49a59c36ad10c8fffdb86
jbzdarkid <jbzdarkid@gmail.com>
2015-09-17 10:00:08 -0400

    Updated to allow access to www.twitch.tv

--- twitch-redirect.user.js
+++ twitch-redirect.user.js
@@ -1,20 +1,23 @@
 // ==UserScript==
 // @match http://*.twitch.tv/*
 // @match http://*.twitch.tv/*/v/*
-// @version 1.2
+// @version 1.3
 // ==/UserScript==
-host = window.location.protocol+"//"+window.location.host+"/"
-path = window.location.pathname.split('/');
+if (window.location.pathname == "/") {
+  return;
+}
+var host = window.location.protocol+"//"+window.location.host+"/"
+var path = window.location.pathname.split('/');
 var url = "";
 if (path.length == 2) {
   url = host+path[1]+"/popout";
 } else if (path.length == 4) {
   url = host+path[1]+"/popout?videoId="+path[2]+path[3];
-  if (window.location.search != "") {
+  if (window.location.search !== "") {
     url += "&"+window.location.search.slice(1); // Cuts initial ?
   }
 }
-if (url != "") {
+if (url !== "") {
   var redir = "\n<meta http-equiv=\"refresh\" content=\"0; url="+url+"\">";
   document.head.innerHTML += redir;
 }
