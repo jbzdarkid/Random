26ee625b7e60ac6476dd01860f26413f5cf0a34c
jbzdarkid <jbzdarkid@gmail.com>
2015-12-16 19:36:35 -0500

    Move items to main
    Also add http:, since bandcamp seems to leave that off now.

--- Bandcamp download.py
+++ Bandcamp download.py
@@ -11,31 +11,6 @@ from os import chdir, mkdir
 import string
 valid_chars = '-_.() %s%s' % (string.ascii_letters, string.digits)
 
-try:
-	mkdir('Bandcamp')
-except OSError:
-	pass
-finally:
-	chdir('Bandcamp')
-lock = Lock()
-data = urlopen('https://harrycallaghan.bandcamp.com/album/tier-3-soundtrack').read()
-m = search('<link rel="image_src" href="(.*?)">', data)
-urlretrieve(m.group(1), 'Cover.jpg')
-m = search('trackinfo :(.*),', data)
-dict = loads(m.group(1))
-numTracks = len(dict)
-global gui
-gui = ' '*numTracks
-threads = [None] * numTracks
-
-# Knuth randomization algorithm
-order = range(numTracks)
-for i in range(numTracks):
-	j = randint(i, numTracks-1)
-	temp = order[i]
-	order[i] = order[j]
-	order[j] = temp
-
 def replace(string, char, location):
 	if location == 0:
 		return char + string[1:]
@@ -50,18 +25,44 @@ def download(url, title, i):
 	gui = replace(gui, ' ', i)
 	lock.release()
 
-for i in order:
-	track = dict[i]
-	print replace(gui, 'v', i)
-	gui = replace(gui, '|', i)
-	title = '{0:02d}-{name}.mp3'.format(i+1, name=track['title'])
-	thread = Thread(target=download, kwargs={
-		'url': track['file']['mp3-128'],
-		'title': title,
-		'i': i})
-	threads[i] = thread
-	thread.start()
-	sleep(0.8)
+if __name__ == "__main__":
+	try:
+		mkdir('Bandcamp')
+	except OSError:
+		pass
+	finally:
+		chdir('Bandcamp')
+	lock = Lock()
+	data = urlopen('https://harrycallaghan.bandcamp.com/album/tier-3-soundtrack').read()
+	m = search('<link rel="image_src" href="(.*?)">', data)
+	urlretrieve(m.group(1), 'Cover.jpg')
+	m = search('trackinfo :(.*),', data)
+	dict = loads(m.group(1))
+	numTracks = len(dict)
+	global gui
+	gui = ' '*numTracks
+	threads = [None] * numTracks
 
-for thread in threads:
-	thread.join()
+	# Knuth randomization algorithm
+	order = range(numTracks)
+	for i in range(numTracks):
+		j = randint(i, numTracks-1)
+		temp = order[i]
+		order[i] = order[j]
+		order[j] = temp
+
+	for i in order:
+		track = dict[i]
+		print replace(gui, 'v', i)
+		gui = replace(gui, '|', i)
+		title = '{0:02d}-{name}.mp3'.format(i+1, name=track['title'])
+		thread = Thread(target=download, kwargs={
+			'url': 'http:'+track['file']['mp3-128'],
+			'title': title,
+			'i': i})
+		threads[i] = thread
+		thread.start()
+		sleep(0.8)
+
+	for thread in threads:
+		thread.join()
