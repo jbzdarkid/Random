1cb299cfd11d022268d4202fbff48757a94d0718
jbzdarkid <jbzdarkid@gmail.com>
2016-05-13 03:45:25 -0400

    Fixed a bug in nCr, added nPr.

--- Multi_iter.py
+++ Multi_iter.py
@@ -3,11 +3,13 @@ def nCr(n, r):
 	for i in range(r):
 		arr.append(i)
 	yield arr
-	
-	i = size-1
+	if r == n:
+		return
+
+	i = r-1
 	while i >= 0:
 		arr[i] += 1
-		while i < size - 1:
+		while i < r-1:
 			i += 1
 			arr[i] = arr[i-1] + 1
 		yield arr
@@ -16,3 +18,17 @@ def nCr(n, r):
 			yield arr
 		while arr[i] == n + i - r:
 			i -= 1
+
+def nPr(n, r):
+	for set in nCr(n, r):
+		for p in permute(set):
+			yield p
+
+def permute(set):
+	if len(set) == 0:
+		yield []
+	for elem in set:
+		subset = list(set)
+		subset.remove(elem)
+		for p in permute(subset):
+			yield [elem] + p
