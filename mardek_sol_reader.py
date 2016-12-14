0604413fc9292289dd17509d5936e3bdafab388d
jbzdarkid <jbzdarkid@gmail.com>
2016-12-14 09:19:40 -0500

    Using regex matching because it's faster and simpler

--- mardek_sol_reader.py
+++ mardek_sol_reader.py
@@ -1,6 +1,7 @@
 from struct import unpack
 from os import walk, sep
 from os.path import expanduser
+from re import search
 
 shared_objects_dirs = [
 	'Library/Application Support/Google/Chrome/Default/Pepper Data/Shockwave Flash/WritableRoot/#SharedObjects',
@@ -16,20 +17,9 @@ for shared_objects_dir in shared_objects_dirs:
 			if filename[:13] == 'MARDEKv3__sg_':
 				savefiles.append([root.split(sep)[-1], int(filename[13:-4]), root+sep+filename])
 
-signal = 'playtime\x08\x00\x00\x00\x03' # 8 represents array, 0003 means 3 elements
 for file in sorted(savefiles):
-	try:
 		f = open(file[2]).read()
-		match = 0
-		for i in range(len(f)-len(signal)):
-			if f[i] != signal[match]:
-				match = 0
-			else:
-				match += 1
-				if match == len(signal):
-					time = ''.join(f[i+1:i+37])
-					h, m, s = unpack('>4xd4xd4xd', time)
-					print 'Save file {:d} from {:s}:\t{:02.0f}:{:02.0f}:{:02.0f}'.format(file[1],file[0],h,m,s)
-					raise IOError
-	except IOError:
-		continue
+		# 8 represents array, 0003 means 3 elements
+		m = search('playtime\x08\x00\x00\x00\x03(.{36})', f)
+		print 'Save file %d from %s:' % (file[1], file[0]),
+		print '\t%d:%02.0f:%02.0f' % unpack('>4xd4xd4xd', m.group(1))
