f19db7b224e0f18d4d89682698e5b9e9d9f811a7
jbzdarkid <jbzdarkid@gmail.com>
2016-12-19 01:24:07 -0500

    Rotated the pieces so that they all default to 0
    Removed commas in the input to start cleaning that up

--- tetrominos.py
+++ tetrominos.py
@@ -4,53 +4,53 @@ checks = [0, 0, 0, 0]
 tetrominos = {
   ('I', 0):
     (0,
+    [[1, 1, 1, 1]]),
+  ('I', 1):
+    (0,
     [[1],
      [1],
      [1],
      [1]]),
-  ('I', 1):
-    (0,
-    [[1, 1, 1, 1]]),
   ('I', 2): ('!', 'Invalid'),
   ('I', 3): ('!', 'Invalid'),
 #----------------------------
   ('J', 0):
-    (1,
-    [[0, 1],
-     [0, 1],
-     [1, 1]]),
-  ('J', 1):
     (0,
     [[1, 0, 0],
      [1, 1, 1]]),
-  ('J', 2):
+  ('J', 1):
     (0,
     [[1, 1],
      [1, 0],
      [1, 0]]),
-  ('J', 3):
+  ('J', 2):
     (0,
     [[1, 1, 1],
      [0, 0, 1]]),
+  ('J', 3):
+    (1,
+    [[0, 1],
+     [0, 1],
+     [1, 1]]),
 #----------------------------
   ('L', 0):
     (0,
-    [[1, 0],
-     [1, 0],
-     [1, 1]]),
-  ('L', 1):
-    (0,
     [[1, 1, 1],
      [1, 0, 0]]),
-  ('L', 2):
+  ('L', 1):
     (0,
     [[1, 1],
      [0, 1],
      [0, 1]]),
-  ('L', 3):
+  ('L', 2):
     (2,
     [[0, 0, 1],
      [1, 1, 1]]),
+  ('L', 3):
+    (0,
+    [[1, 0],
+     [1, 0],
+     [1, 1]]),
 #----------------------------
   ('O', 0):
     (0,
@@ -412,7 +412,7 @@ def solve(challenges, NUMTHREADS, _MAXSOLNS, benchmark=False):
     data = challenges[title]
     maxCost = -1
     solutions = []
-    pieces = data[0].split(', ')
+    pieces = data[0].split(' ')
 
     TCount = 0
     for i in range(len(pieces)):
@@ -462,25 +462,27 @@ def solve(challenges, NUMTHREADS, _MAXSOLNS, benchmark=False):
 if __name__ == "__main__":
   challenges = {
     # 'Name': ['Pieces', Height, Width],
-    'Connector':  ['T0, T0, L1', 3, 4], # 1
-    'Cube':       ['T0, T0, L1, Z0', 4, 4], # 1
-    'Fan':        ['T0, T0, L1, S0, Z0', 5, 4], # 1
-    'Recorder':   ['T0, T0, J1, S0, Z0', 5, 4], # 0
-    'Platform':   ['I1, O0, T0, T0, L1, Z0', 6, 4], # 3.1
-    'A':          ['I1, L1, J1, Z0', 4, 4], # 0
-    'B':          ['I1, T0, T0, L1, Z0', 5, 4], # 1
-    'C':          ['T0, T0, J1, J1, L1, Z0', 6, 4], # 0
-    'A star':     ['T0, T0, T0, T0, L1, J1, S0, S0, Z0, Z0', 5, 8], # 1
-    'B star':     ['I1, I1, O0, T0, T0, T0, T0, L1, L1, J1', 5, 8], # 1
-    'C star':     ['L1, J1, S0, Z0, T0, T0, I1, I1, O0, O0', 5, 8], # 1
-    'Floor 1':    ['L1, Z0, L1, Z0', 4, 4], # 1
-    'Floor 2':    ['O0, T0, T0, T0, T0, L1, L1, L1, L1', 6, 6], # 2
-    'Floor 3':    ['I1, I1, I1, I1, J1, J1, L1, L1, S0, Z0', 5, 8], # 0
-    'Floor 4':    ['O0, O0, T0, T0, T0, T0, J1, L1, S0, S0, Z0, Z0', 8, 6], # 1
-    'Floor 5':    ['I1, I1, O0, O0, O0, O0, T0, T0, T0, T0, J1, L1, S0, Z0', 7, 8], # 0
-    'Floor 6':    ['O0, S0, S0, S0, S0, L0, L0, L0, L0', 6, 6], # 3.1
-    'DLC Silver': ['I1, T0, T0, L1, L1, J1, J1', 4, 7], # 1
-    'DLC Gold':   ['T0, S0, S0, T0, T0, I1, S0, I1, T0, L1', 8, 5], # 3.1
+    # 'Connector':  ['T0 T0 L0, 3, 4], # 1
+    # 'Hexahedron': ['T0 T0 L0 Z0', 4, 4], # 1
+    # 'Fan':        ['T0 T0 L0 S0 Z0', 5, 4], # 1
+    # 'Recorder':   ['T0 T0 J0 S0 Z0', 5, 4], # 0
+    # 'Platform':   ['I0 O0 T0 T0 L0 Z0', 6, 4], # 3.1
+    # 'A':          ['I0 L0 J0 Z0', 4, 4], # 0
+    # 'B':          ['I0 T0 T0 L0 Z0', 5, 4], # 1
+    # 'C':          ['T0 T0 J0 J0 L0 Z0', 6, 4], # 0
+    # 'A star':     ['T0 T0 T0 T0 L0 J0 S0 S0 Z0 Z0', 5, 8], # 1
+    # 'B star':     ['I0 I0 O0 T0 T0 T0 T0 L0 L0 J0', 5, 8], # 1
+    # 'C star':     ['L0 J0 S0 Z0 T0 T0 I0 I0 O0 O0', 5, 8], # 1
+    # 'Floor 1':    ['L0 Z0 L0 Z0', 4, 4], # 1
+    # 'Floor 2':    ['O0 T0 T0 T0 T0 L0 L0 L0 L0', 6, 6], # 2
+    # 'Floor 3':    ['I0 I0 I0 I0 J0 J0 L0 L0 S0 Z0', 5, 8], # 0
+    # 'Floor 4':    ['O0 O0 T0 T0 T0 T0 J0 L0 S0 S0 Z0 Z0', 8, 6], # 1
+    # 'Floor 5':    ['I0 I0 O0 O0 O0 O0 T0 T0 T0 T0 J0 L0 S0 Z0', 7, 8], # 0
+    # 'Floor 6':    ['O0 S0 S0 S0 S0 L0 L0 L0 L0', 6, 6], # 3.1
+
+
+    # 'Gehenna Silver': ['I0, T0, T0, L0, L0, J0, J0', 4, 7], # 1
+    # 'Gehenna Gold':   ['T0, S0, S0, T0, T0, I0, S0, I0, T0, L0', 8, 5], # 3.1
   }
   solve(challenges, 16, 0)
   if DEBUG:
