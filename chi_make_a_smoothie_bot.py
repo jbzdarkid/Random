5127914f6842e77e9a1613b58ff28fae420b0da8
jbzdarkid <jbzdarkid@gmail.com>
2020-04-13 22:56:47 -0700

    Create chi_make_a_smoothie_bot.py

---
+++ chi_make_a_smoothie_bot.py
@@ -0,0 +1,47 @@
+from asyncio import sleep
+import discord
+import subprocess
+import time
+from datetime import datetime
+import sys
+from pathlib import Path
+
+client = discord.Client()
+client.started = False
+
+@client.event
+async def on_ready():
+  if client.started: # @Hack: Properly deal with disconnection / reconnection
+    await client.close()
+    return
+  client.started = True
+
+  print(f'Logged in as {client.user.name} (id: {client.user.id})')
+
+  channel = client.get_channel(699493553444749367)
+  sent_today = False
+  while 1:
+    now = datetime.now()
+    tomorrow = datetime(now.year, now.month, now.day + 1)
+    def is_a_workday(day):
+      return day.weekday() >= 0 and day.weekday() <= 4
+
+    if is_a_workday(tomorrow) and not sent_today and now.hour == 18: # 6 PM
+      sent_today = True
+      message = await channel.send(content='Chi! Make a smoothie.')
+
+    await sleep(60)
+
+  await client.close()
+
+if __name__ == '__main__':
+  if 'subtask' not in sys.argv:
+    while 1:
+      subprocess.run([sys.executable, __file__, 'subtask'] + sys.argv)
+      # Speedrun.com throttling limit is 100 requests/minute
+      time.sleep(60)
+  else:
+    with open(Path(__file__).parent / 'chi_make_a_smoothie_token.txt', 'r') as f:
+      token = f.read().strip()
+    client.run(token, reconnect=True)
+
