59ebb2730cb3ab2f019d4d4c7503332b7ba778c2
jbzdarkid <jbzdarkid@gmail.com>
2024-03-03 15:40:58 -0600

    Two tests working now

--- hs_cards.py
+++ hs_cards.py
@@ -83,7 +83,7 @@ def normalize_deck(cards):
   return normalized_cards
 
 def bad_hash(x, val=0):
-  return x ^ (val + 2654435769 + (x << 6) + (x >> 2))
+  return x ^ trunc(val + 2654435769 + (x << 6) + (x >> 2))
 
 def hash_deck(cards):
   hash = 0
@@ -105,14 +105,14 @@ def bits(x, bits=64):
   return '0' * (bits - len(str)) + str
 
 
-def reverse_hash(y, val):
+def reverse_hash(y, val, debug=False, x=None):
   # We don't have any real data about the first two bits, but usually they will trial-and-error away.
   for initial_guess in [0b00, 0b01, 0b10, 0b11]:
     guess = initial_guess
     for j in range(2, 64):
       mask = (1 << (j - 1)) - 1
       bit = 1 << j
-
+      
       if (bad_hash(guess, val) & mask) == (y & mask):
         continue
       elif (bad_hash(guess | bit, val) & mask) == (y & mask):
@@ -120,46 +120,44 @@ def reverse_hash(y, val):
         continue
       break
     # Double check in case we exited early (also verifies the high 2 bits)
-    if bad_hash(guess, val) != y:
+    if bad_hash(trunc(guess), val) != y:
+      if debug:
+        print('For initial guess', initial_guess)
+        print(bits(guess), guess, 'guess')
+        print(bits(x), x, 'x')
+        print(bits(y), y, 'y')
+        print(bits(bad_hash(x, val)), bad_hash(x, val), 'hash')
       continue
     return guess
   return None
 
 
-"""
-import random
-for i in range(100):
-  print('-'*10, 'Attempt', i+1)
-  x = random.randint(2 ** 63, 2 ** 64)
-  v = random.randint(1, 1000)
-  y = bad_hash(x, v)
-  guess = reverse_hash(y, v)
-  print('Guessed x', bits(guess))
-  print('Actual x ', bits(x))
-  assert(guess == x)
-"""
+def test1():
+  import random
+  seed = random.randint(0, 1 << 64)
+  print('seed =', seed)
+  for i in range(100):
+    x = random.randint(-1 << 63, 1 << 63)
+    v = random.randint(1, 1000)
+    y = bad_hash(x, v)
+    guess = reverse_hash(y, v)
+    if guess == None:
+      reverse_hash(y, v, debug=True, x=x)
+      assert False
+      
+  print('Test1 passed')
 
-print('Something bad happening')
-x = 191242499615597045949
-y = 12106176445390699504078
-
-print(bad_hash(x, 46667))
-print(y)
-
-x2 = reverse_hash(y, 46667)
-print(x2)
-exit()
-
-
-hash = 0
-for i, card in enumerate(decklist):
-  print('-'*50, 'i', i)
-  print('pre hash', hash)
-  print('card', card)
-  hash = bad_hash(hash, card)
-  print('post hash', hash)
-  rhash = reverse_hash(hash, card)
-  print('rhash', rhash)
-  assert rhash is not None
-  print('rebuild', bad_hash(rhash, card))
+def test2():
+  hash = 0
+  cards = decode_decklist('AAEBAaoIDLSKBLaKBKyfBNugBOCgBJbUBKDUBKnUBPzbBMviBJakBfCuBQmf1ASo2QS12QT03ASz3QS14gSl5ATF7QTK7QQA')
+  for card in cards:
+    hash = bad_hash(hash, card)
+    rhash = reverse_hash(hash, card)
+    assert rhash is not None
+    # rhash will not always exactly match hash
+  print('Test2 passed')
+    
+if __name__ == '__main__':
+  test1()
+  test2()
   
