c946542ccf6d8dee4a69f78605ee0c1c25812b7b
jbzdarkid <jbzdarkid@gmail.com>
2025-02-23 17:17:02 -0800

    All working

--- srcom/srcom_twitch_highlights.py
+++ srcom/srcom_twitch_highlights.py
@@ -1,36 +1,62 @@
-import requests
-import datetime
+import sys
 import utils
-import time
 
-# src_id = utils.search_src_user(input('Enter your Speedrun.com username: '))
-# print(f'Found Speedrun.com user {src_id}')
-# runs = utils.get_runs(user=src_id, orderby='date', direction='asc')
+from SpeedrunBot.source import src_apis
 
-game_id = utils.get_game_id(input('Enter the Speedrun.com game: '))
-print(f'Found Speedrun.com game {game_id}')
+if len(sys.argv) < 1:
+  game_name = input('Enter the Speedrun.com game: ')
+else:
+  game_name = sys.argv[1]
+game_id = utils.get_game_id(game_name)
+print(f'Parsing Speedrun.com game {game_name} ({game_id})')
 
-categories = utils.get_categories(game_id)
-for category in categories:
-  runs = utils.get_runs(game=game_id, category=category, orderby='date', direction='asc')
 
-  # There are two kinds of runs that we care about retaining: PBs and WRs.
-  # For PBs, we just care about the best time per user. (iterate all runs, track best per user)
-  # For WRs, we care about the best run at a given moment in time. (iterate all runs in order, track decrementing times)
-  pbs = {}
-  wrs = []
+# There are two kinds of runs that we care about retaining: PBs and WRs.
+# For PBs, we just care about the best time per user. (iterate all runs, track best per user)
+# For WRs, we care about the best run at a given moment in time. (iterate all runs in order, track decrementing times)
+data = {}
+runs = utils.get_runs(game=game_id, orderby='date', direction='asc')
+for run in runs:
+  category = utils.get_full_category(run)
+  if category not in data:
+    data[category] = {'pbs': {}, 'wrs': []}
+  pbs = data[category]['pbs']
+  wrs = data[category]['wrs']
 
-  for run in runs:
-    players = set(parse_name(player) for player in new_run['players']['data'])
-    run_time = run['times']['primary_t']
-    if players not in pbs:
-      pbs[players] = run
-    elif run_time < pbs[players]['times']['primary_t']:
-      pbs[players] = run
+  players = ', '.join([src_apis.parse_name(player) for player in run['players']['data']])
+  run_time = run['times']['primary_t']
+  if players not in pbs:
+    pbs[players] = run
+  elif run_time < pbs[players]['times']['primary_t']:
+    pbs[players] = run
+    
+  if len(wrs) == 0:
+    wrs.append(run)
+  elif run_time < wrs[-1]['times']['primary_t']:
+    wrs.append(run)
 
-    if run_time < wrs[-1]['times']['primary_t']:
-      wrs.append(run)
+for category in sorted(data.keys()):
+  print('\t\tCategory', category)
 
-  print(pbs)
-  print(wrs)
-  input()
+  pbs = list(data[category]['pbs'].values())
+  pbs.sort(key = lambda run: run['times']['primary_t'])
+  print(f'\tCurrent leaderboard ({len(pbs)} PBs):')
+  for run in pbs:
+    video = run['videos']['links'][0]['uri']
+    if 'twitch' in video:
+      print('[TWITCH VOD] ', end='')
+    print(utils.run_to_short_string(run))
+
+  wrs = data[category]['wrs']
+  print(f'\tWR timeline ({len(wrs)} WRs):')
+  for run in wrs:
+    if 'text' in run['videos']:
+      video = run['videos']['text']
+    elif 'links' in run['videos']:
+      video = run['videos']['links'][0]['uri']
+    else:
+      video = None
+
+    if 'twitch' in video:
+      print('[TWITCH VOD] ', end='')
+    print(utils.run_to_short_string(run))
--- srcom/utils.py
+++ srcom/utils.py
@@ -1,87 +1,60 @@
+from datetime import timedelta
 import requests
+
+from SpeedrunBot.source import src_apis
+api = src_apis.api
 
 def login():
   # https://www.speedrun.com/darkid/settings/api
   headers = {'X-API-Key': input('X-API-Key: ')}
-  r = requests.get('https://www.speedrun.com/api/v1/profile', headers=headers)
+  r = requests.get(f'{api}/profile', headers=headers)
   if r.status_code != 200:
     raise ValueError('Failed to authenticate')
-  print('Authenticated as ' + r.json()['data']['names']['international'])
+  print('Authenticated as ' + src_apis.parse_name(r.json()['data']))
   return headers
-
-def get_runs(**params):
-  params['offset'] = 0
-  params['max'] = 100 # Undocumented parameter, gets 100 runs at once.
-  if not (params.keys() & {'game', 'category', 'user'}):
-    raise ValueError('You need a primary search type to get runs from Speedrun.com')
-
-  runs = []
-  j = requests.get('https://www.speedrun.com/api/v1/runs', params=params).json()
-  while 1:
-    runs += j['data']
-    if 'limit' in params and len(runs) > params['limit']:
-      break
-
-    for link in j['pagination']['links']:
-      if link['rel'] == 'next':
-        j = requests.get(link['uri']).json()
-        continue
-    break # No more results
-
-  return runs
-
 
 names = {}
 def get_name(player_id):
   if player_id not in names:
-    j = requests.get(f'https://www.speedrun.com/api/v1/users/{player_id}').json()
-    names[player_id] = j['data']['names']['international']
+    j = requests.get(f'{api}/users/{player_id}').json()
+    names[player_id] = src_apis.parse_name(j['data'])
 
   return names[player_id]
 
 def get_category_name(category_id):
-  j = requests.get(f'https://www.speedrun.com/api/v1/categories/{category_id}').json()
+  j = requests.get(f'{api}/categories/{category_id}').json()
   return j['data']['name']
 
-def is_video_unlisted(video_url):
-  # allow_redirects=True
-  r = requests.get(video_url)
-  return '<meta itemprop="unlisted" content="True">' in r.text
+def get_categories(game_id):
+  j = requests.get(f'{api}/games/{game_id}/categories').json()
+  return j['data']
 
-# From SpeedrunBot
+# The first half of src_apis.run_to_string
+def run_to_short_string(run):
+  category = run['category']['data']['name']
+
+  if isinstance(run['level']['data'], dict):
+    category = run['level']['data']['name'] + f': {category}'
+
+  subcategories = src_apis.get_subcategories(run)
+  for value in subcategories.values():
+    category += f' ({value["label"]})'
+
+  time = timedelta(seconds=run['times']['primary_t'])
+  runners = ', '.join(map(src_apis.parse_name, run['players']['data']))
+  return f'{time} by {runners}: {run["weblink"]}'
+
+def get_runs(**params):
+  return src_apis.get_runs(**params)
+
 def search_src_user(username):
-  j = requests.get('https://www.speedrun.com/api/v1/users', params={'name': username}).json()
-  if len(j['data']) == 0:
-    raise ValueError(f'Could not find user {username} on Speedrun.com')
-
-  if len(j['data']) == 1:
-    return j['data'][0]['id']
-
-  possible_matches = []
-  for user in j['data']:
-    possible_match = user['names']['international']
-    if possible_match == username:
-      return user['id']
-    possible_matches.append(possible_match)
-
-  suggestions = ', '.join(possible_matches[:10]) # Only show a max of 10 matches, for brevity's sake
-  raise ValueError(f'Found {len(possible_matches)} possible matches for user {username} on Speedrun.com -- Try one of these options:\n' + suggestions)
+  return src_apis.search_src_user(username)
 
 def get_game_id(game_name):
-  j = requests.get('https://www.speedrun.com/api/v1/games', params={'name': game_name}).json()
-  if len(j['data']) == 0:
-    raise ValueError(f'Could not find game {game_name} on Speedrun.com')
+  return src_apis.get_game(game_name)['id']
 
-  if len(j['data']) == 1:
-    return j['data'][0]['id']
-
-  possible_matches = []
-  for game in j['data']:
-    possible_match = game['names']['twitch']
-    if possible_match == game_name:
-      return game['id']
-    possible_matches.append(possible_match)
-
-  suggestions = ', '.join(possible_matches[:10]) # Only show a max of 10 matches, for brevity's sake
-  raise ValueError(f'Found {len(possible_matches)} possible matches for game {game_name} on Speedrun.com -- Try one of these options:\n' + suggestions)
-
+def get_full_category(run):
+  category = run['category']['data']['name']
+  for subcategory in src_apis.get_subcategories(run).values():
+    category += ' (' + subcategory['label'] + ')'
+  return category
